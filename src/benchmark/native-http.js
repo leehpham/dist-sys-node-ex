15157bf34dde15850658cda7b2893c2a4062f333
Hoang Pham <hoang.pham@enouvo.com>
2024-09-26 17:50:41 +0700

    chore: updated.

---
+++ src/benchmark/native-http.js
@@ -0,0 +1,10 @@
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 4000;
+
+require("http")
+  .createServer((req, res) => {
+    res.end("ok");
+  })
+  .listen(PORT, () => {
+    console.log(`Producer running at http://${HOST}:${PORT}`);
+  });
